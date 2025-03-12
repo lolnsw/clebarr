6a5755aff720c9eab7f03b7e15aade32e9a20e66
lol <lol@Mac-mini-M2-Pro.local>
2025-03-12 10:36:07 +0800

    Actions fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="clebarr",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(include=["app", "app.*"]),
     python_requires=">=3.11",
     install_requires=[
         line.strip()
